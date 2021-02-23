d6d49886e75a7e30672eddf7232e3f1f4efe93ec
Rachel Padworski <rachelpadworski@gmail.com>
2021-02-23 16:12:39 -0700

    checks if author field is blank

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const TwitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    
+    // Check if Author field is blank/null and replace it with 'Unkown'
+    if (!quote.author){
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // Check Quote length to determine the styling
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -23,6 +43,7 @@ async function getQuotes() {
 // On load
 getQuotes();
 
+//////////
 
 // // Get quote from API
 // async function getQuote() {
