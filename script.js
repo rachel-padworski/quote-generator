fe4e1ad38a931c2708c6d394e6e43eb78772adfd
Rachel Padworski <rachelpadworski@gmail.com>
2021-02-23 20:08:02 -0700

    adds loading spinner

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const TwitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+
+function hideLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+    showLoadingSpinner();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
@@ -24,24 +37,37 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    hideLoadingSpinner();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    showLoadingSpinner();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
     } catch (error) {
-
-        // Catch Error Here
+        getQuotes();
     }
 }
 
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+TwitterBtn.addEventListener('click', tweetQuote);
+
 // On load
 getQuotes();
+
 
 //////////
 
